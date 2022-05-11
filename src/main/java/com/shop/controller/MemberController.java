9027bb3edb3687e0dd8a13ea87034f10f5d2ea29
unknown <yooooonnf@gmail.com>
2022-05-11 23:17:44 +0900

    shop

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -1,22 +1,52 @@
 package com.shop.controller;
 
 import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
 
     @GetMapping("/new")
     public String MemberFrom(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
+
+    @PostMapping("/new")
+    public String memberForm(@Validated @ModelAttribute MemberFormDto memberFormDto,
+                             BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult발견");
+            return "member/memberForm";
+        }
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+
+
 }
