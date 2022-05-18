ac68331281c1dbc2a4903d044f6eee375ad2416a
unknown <yooooonnf@gmail.com>
2022-05-18 23:12:23 +0900

    shop

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public CartItem() {
+
+    }
+}
