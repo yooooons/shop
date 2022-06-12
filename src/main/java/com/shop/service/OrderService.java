ba4b39e07fe356d6103ee632c458eaebdfcf5e63
unknown <yooooonnf@gmail.com>
2022-06-12 22:56:36 +0900

    shop

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,18 +1,22 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repo.ItemImgRepository;
 import com.shop.repo.ItemRepository;
 import com.shop.repo.MemberRepository;
 import com.shop.repo.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +27,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long saveOrder(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId())
@@ -38,8 +43,26 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
-
-
     }
 
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+            orderHistDtos.add(orderHistDto);
+        }
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+    }
 }
--- src/test/java/com/shop/service/OrderServiceTest.java
+++ src/test/java/com/shop/service/OrderServiceTest.java
@@ -1,14 +1,27 @@
 package com.shop.service;
 
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.OrderDto;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import com.shop.entity.Order;
+import com.shop.entity.OrderItem;
 import com.shop.repo.ItemRepository;
 import com.shop.repo.MemberRepository;
 import com.shop.repo.OrderRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,13 +32,51 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
     @Autowired
-    private OrderRepository repository;
+    private OrderRepository orderRepository;
     @Autowired
     ItemRepository itemRepository;
     @Autowired
     MemberRepository memberRepository;
 
+    public Item saveItem() {
+        Item item = Item.builder()
+                .itemNm("테스트 상품")
+                .price(10000)
+                .stockNumber(100)
+                .itemSellStatus(ItemSellStatus.SELL)
+                .itemDetail("테스트 상세 설명입니다.")
+                .build();
+        return itemRepository.save(item);
+    }
 
+    public Member saveMember() {
+        Member member = Member.builder()
+                .email("test@test")
+                .build();
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    void order() {
+        //given
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+        //when
+        Long orderId = orderService.saveOrder(orderDto, member.getEmail());
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+        List<OrderItem> orderItems = order.getOrderItems();
+        int totalPrice = orderDto.getCount() * item.getPrice();
+
+        //then
+        assertThat(totalPrice).isEqualTo(order.getTotalPrice());
+
+    }
 
 
 }
