5f9490de0bbd51dfc7963f21b45a202164caa8a8
unknown <yooooonnf@gmail.com>
2022-06-13 21:58:49 +0900

    shop

--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -1,22 +1,33 @@
 package com.shop.controller;
 
 import com.shop.dto.OrderDto;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
 import com.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
     private final OrderService orderService;
 
@@ -42,6 +53,25 @@ public class OrderController {
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
 
+    }
+
+    @GetMapping({"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable Optional<Integer> page, Principal principal, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+
+        return "order/orderHist";
 
     }
+
+
 }
