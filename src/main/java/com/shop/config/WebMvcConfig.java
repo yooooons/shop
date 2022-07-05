7a35928d7be29780f6bac6bc5a557d4741910687
unknown <yooooonnf@gmail.com>
2022-07-05 18:31:41 +0900

    argument resolver 등록

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package com.shop.config;
 
+import com.shop.config.argumentResolver.LoginMemberArgumentResolver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -14,4 +18,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
     }
+
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
 }
